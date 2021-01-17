38437cad2335be64fd190ea86335b9b33c86deb6
GlucoScan <controlbetter@gmail.com>
2021-01-17 01:00:01 +0000

    Updates

---
+++ precache-manifest.3dfce05ee8b8721394f2b7da92855e2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e3dfc0e47d94f435d71eb9cb6cfc32",
+    "url": "/myrobofriends/index.html"
+  },
+  {
+    "revision": "ef55fdd545a4dc77ac12",
+    "url": "/myrobofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7de156f08c6af047b328",
+    "url": "/myrobofriends/static/css/main.0743e268.chunk.css"
+  },
+  {
+    "revision": "ef55fdd545a4dc77ac12",
+    "url": "/myrobofriends/static/js/2.3f267d07.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/myrobofriends/static/js/2.3f267d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de156f08c6af047b328",
+    "url": "/myrobofriends/static/js/main.2a70379d.chunk.js"
+  },
+  {
+    "revision": "a24a4b5cdcaa39f79055",
+    "url": "/myrobofriends/static/js/runtime-main.d5482856.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/myrobofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
